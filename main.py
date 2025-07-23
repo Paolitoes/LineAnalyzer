baebf1e835223720609f0d530d91447cc393978e
Paolitoes <carlospaolo15@gmail.com>
2025-07-23 11:40:48 -0400

    Testing erosion for robust dot detection

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ img = cv.imread("Ghost1.png",cv.IMREAD_GRAYSCALE)
 if img is None:
     sys.exit("Could not read the image.")
 
-#dst = cv.addWeighted(img,0.7,img,0.3,0)
+thresh = lambda x: cv.threshold(x,115,255,cv.THRESH_BINARY)
+
+def dotdetect(img1):
+    temp = cv.GaussianBlur(img1,(7,7),0)
+    ret2, temp = thresh(temp)
+    return temp
+
 
 edges = cv.Canny(img,100,200)
-ret, dots = cv.threshold(img,127,255,cv.THRESH_BINARY)
+dots = dotdetect(img)
+ret, thrImg = thresh(img)
+erosion = cv.erode(255-thrImg,np.ones((5,5),np.int8))
+
+cv.rectangle(img,(0,0),(7,7),0)
 
 cv.imshow("Display window", img)
 cv.imshow("Edges",edges)
 cv.imshow("Dots",dots)
+cv.imshow("Erosion",erosion)
 
 k = cv.waitKey(0)
 
