590b627860ab804260a75595c84000da5773da26
Paolitoes <carlospaolo15@gmail.com>
2025-07-24 18:26:55 -0400

    Feature analysis Pseudo overlaid

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ img = cv.imread("Ghost1.png",cv.IMREAD_GRAYSCALE)
 if img is None:
     sys.exit("Could not read the image.")
 
-thresh = lambda x: cv.threshold(x,115,255,cv.THRESH_BINARY)
+def thresh(x):
+    _, thr = cv.threshold(x,127,255,cv.THRESH_BINARY)
+    return thr
 
+fin = np.zeros(img.shape+(3,))
+fin[:,:,0] = thresh(img)
+fin[:,:,1] = cv.Canny(img,100,200)
+
+cv.imshow("Overlay Window",fin)
+
+
+'''
 def dotdetect(img1):
     temp = cv.GaussianBlur(img1,(7,7),0)
     ret2, temp = thresh(temp)
@@ -24,10 +34,10 @@ erosion = cv.erode(255-thrImg,np.ones((4,4),np.int8))
 cv.rectangle(img,(0,0),(7,7),0)
 
 cv.imshow("Display window", img)
-# cv.imshow("Edges",edges)
-# cv.imshow("Dots",dots)
+cv.imshow("Edges",edges)
+cv.imshow("Dots",dots)
 cv.imshow("Erosion",erosion)
-
+'''
 k = cv.waitKey(0)
 
 if k ==ord("s"):
